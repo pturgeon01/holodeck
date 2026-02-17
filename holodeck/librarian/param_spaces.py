cf982b1d3efe8cc7d603dca2a0c7f2603c1b5f3e
Emiko C Gardiner <ecg@berkeley.edu>
2026-02-17 13:54:27 -0800

    Create ECG_17 parameter space in exact state of astro_strong_all on Oct 25, 2024

--- holodeck/librarian/param_spaces.py
+++ holodeck/librarian/param_spaces.py
@@ -895,7 +895,177 @@ class PS_Astro_Strong_ECG_15(_PS_Astro_Strong):
         # self.DEFAULTS['hard_rchar'] = 100.0
         # self.DEFAULTS['hard_gamma_outer'] = 0.0
         return
-       
+
+
+class PS_Astro_Strong_ECG17(_Param_Space):
+    """SAM Model with strongly astrophysically-motivated parameters, in the exact state of PS_Astro_Strong_Hard_All(_PS_Astro_Strong) on 2024_10_25
+
+    This model uses a double-Schechter GSMF, an Illustris-derived galaxy merger rate, a Kormendy+Ho
+    M-MBulge relationship, and a phenomenology binary evolution model.
+
+    """
+
+    __version__ = "0.2"
+
+    DEFAULTS = dict(
+        # Hardening model (phenom 2PL)
+        hard_time=3.0,          # [Gyr]
+        hard_sepa_init=1e4,     # [pc]
+        hard_rchar=10.0,        # [pc]
+        hard_gamma_inner=-1.0,
+        hard_gamma_outer=0.0,
+
+        # Galaxy stellar-mass Function (``GSMF_Double_Schechter``)
+        # Parameters are based on `double-schechter.ipynb` conversions from [Leja2020]_
+        gsmf_log10_phi_one_z0=-2.383,    # - 2.383 ± 0.028
+        gsmf_log10_phi_one_z1=-0.264,    # - 0.264 ± 0.072
+        gsmf_log10_phi_one_z2=-0.107,    # - 0.107 ± 0.031
+        gsmf_log10_phi_two_z0=-2.818,    # - 2.818 ± 0.050
+        gsmf_log10_phi_two_z1=-0.368,    # - 0.368 ± 0.070
+        gsmf_log10_phi_two_z2=+0.046,    # + 0.046 ± 0.020
+        gsmf_log10_mstar_z0=+10.767,     # +10.767 ± 0.026
+        gsmf_log10_mstar_z1=+0.124,      # + 0.124 ± 0.045
+        gsmf_log10_mstar_z2=-0.033,      # - 0.033 ± 0.015
+        gsmf_alpha_one=-0.28,            # - 0.280 ± 0.070
+        gsmf_alpha_two=-1.48,            # - 1.480 ± 0.150
+
+        # Galaxy merger rate (``GMR_Illustris``)
+        # Parameters are taken directly from [Rodriguez-Gomez2015]_
+        gmr_norm0_log10=-2.2287,         # -2.2287 ± 0.0045    A0 [log10(A*Gyr)]
+        gmr_normz=+2.4644,               # +2.4644 ± 0.0128    eta
+        gmr_malpha0=+0.2241,             # +0.2241 ± 0.0038    alpha0
+        gmr_malphaz=-1.1759,             # -1.1759 ± 0.0316    alpha1
+        gmr_mdelta0=+0.7668,             # +0.7668 ± 0.0202    delta0
+        gmr_mdeltaz=-0.4695,             # -0.4695 ± 0.0440    delta1
+        gmr_qgamma0=-1.2595,             # -1.2595 ± 0.0026    beta0
+        gmr_qgammaz=+0.0611,             # +0.0611 ± 0.0021    beta1
+        gmr_qgammam=-0.0477,             # -0.0477 ± 0.0013    gamma
+
+        # M-MBulge Relationship (``MMBulge_KH2013``)
+        # From [KH2013]_
+        mmb_mamp_log10=8.69,             # 8.69±0.05  [log10(M/Msol)]  approx uncertainties!
+        mmb_plaw=1.17,                   # 1.17 ± 0.08
+        mmb_scatter_dex=0.28,            # no uncertainties given
+        # bulge fraction
+        bf_frac_lo=0.4,
+        bf_frac_hi=0.8,
+        bf_mstar_crit=11.0,              # [log10(M_star/M_Sol)]
+        bf_width_dex=1.0,                # [dex]
+    )
+
+    def _init_sam(self, sam_shape, params):
+        log10_phi_one = [
+            params['gsmf_log10_phi_one_z0'],
+            params['gsmf_log10_phi_one_z1'],
+            params['gsmf_log10_phi_one_z2'],
+        ]
+        log10_phi_two = [
+            params['gsmf_log10_phi_two_z0'],
+            params['gsmf_log10_phi_two_z1'],
+            params['gsmf_log10_phi_two_z2'],
+        ]
+        log10_mstar = [
+            params['gsmf_log10_mstar_z0'],
+            params['gsmf_log10_mstar_z1'],
+            params['gsmf_log10_mstar_z2'],
+        ]
+        gsmf = holo.sams.GSMF_Double_Schechter(
+            log10_phi1=log10_phi_one,
+            log10_phi2=log10_phi_two,
+            log10_mstar=log10_mstar,
+            alpha1=params['gsmf_alpha_one'],
+            alpha2=params['gsmf_alpha_two'],
+        )
+
+        # Illustris Galaxy Merger Rate
+        gmr = holo.sams.GMR_Illustris(
+            norm0_log10=params['gmr_norm0_log10'],
+            normz=params['gmr_normz'],
+            malpha0=params['gmr_malpha0'],
+            malphaz=params['gmr_malphaz'],
+            mdelta0=params['gmr_mdelta0'],
+            mdeltaz=params['gmr_mdeltaz'],
+            qgamma0=params['gmr_qgamma0'],
+            qgammaz=params['gmr_qgammaz'],
+            qgammam=params['gmr_qgammam'],
+        )
+
+        # Mbh-MBulge relationship (and bulge-fractions)
+        bulge_frac = holo.host_relations.BF_Sigmoid(
+            bulge_frac_lo=params['bf_frac_lo'],
+            bulge_frac_hi=params['bf_frac_hi'],
+            mstar_char_log10=params['bf_mstar_crit'],
+            width_dex=params['bf_width_dex'],
+        )
+        mmbulge = holo.host_relations.MMBulge_KH2013(
+            mamp_log10=params['mmb_mamp_log10'],
+            mplaw=params['mmb_plaw'],
+            scatter_dex=params['mmb_scatter_dex'],
+            bulge_frac=bulge_frac,
+        )
+
+        sam = holo.sams.Semi_Analytic_Model(
+            gsmf=gsmf, gmr=gmr, mmbulge=mmbulge, shape=sam_shape, log=self._log,
+        )
+        return sam
+
+    def _init_hard(self, sam, params):
+        hard = holo.hardening.Fixed_Time_2PL_SAM(
+            sam,
+            params['hard_time']*GYR,
+            sepa_init=params['hard_sepa_init']*PC,
+            rchar=params['hard_rchar']*PC,
+            gamma_inner=params['hard_gamma_inner'],
+            gamma_outer=params['hard_gamma_outer'],
+        )
+        return hard 
+    
+    def __init__(self, log=None, nsamples=None, sam_shape=None, seed=None):
+        parameters = [
+            # Hardening model (phenom 2PL)
+            PD_Uniform("hard_time", 0.1, 11.0, default=3.0),   # [Gyr]
+            PD_Uniform("hard_gamma_inner", -2.0, +0.0, default=-1.0),
+            PD_Uniform("hard_rchar", 2.0, 20.0, default=10.0),    # [pc]
+
+            # GSMF
+            PD_Normal('gsmf_log10_phi_one_z0', -2.383, 0.028),    # - 2.383 ± 0.028
+            PD_Normal('gsmf_log10_phi_one_z1', -0.264, 0.072),    # - 0.264 ± 0.072
+            PD_Normal('gsmf_log10_phi_one_z2', -0.107, 0.031),    # - 0.107 ± 0.031
+            PD_Normal('gsmf_log10_phi_two_z0', -2.818, 0.050),    # - 2.818 ± 0.050
+            PD_Normal('gsmf_log10_phi_two_z1', -0.368, 0.070),    # - 0.368 ± 0.070
+            PD_Normal('gsmf_log10_phi_two_z2', +0.046, 0.020),    # + 0.046 ± 0.020
+            PD_Normal('gsmf_log10_mstar_z0', +10.767, 0.026),     # +10.767 ± 0.026
+            PD_Normal('gsmf_log10_mstar_z1', +0.124, 0.045),      # + 0.124 ± 0.045
+            PD_Normal('gsmf_log10_mstar_z2', -0.033, 0.015),      # - 0.033 ± 0.015
+            PD_Normal('gsmf_alpha_one', -0.28, 0.070),            # - 0.280 ± 0.070
+            PD_Normal('gsmf_alpha_two', -1.48, 0.150),            # - 1.480 ± 0.150
+
+            # GMR
+            PD_Normal('gmr_norm0_log10', -2.2287, 0.0045),        # -2.2287 ± 0.0045    A0 [log10(A*Gyr)]
+            PD_Normal('gmr_normz', +2.4644, 0.0128),              # +2.4644 ± 0.0128    eta
+            PD_Normal('gmr_malpha0', +0.2241, 0.0038),            # +0.2241 ± 0.0038    alpha0
+            PD_Normal('gmr_malphaz', -1.1759, 0.0316),            # -1.1759 ± 0.0316    alpha1
+            PD_Normal('gmr_mdelta0', +0.7668, 0.0202),            # +0.7668 ± 0.0202    delta0
+            PD_Normal('gmr_mdeltaz', -0.4695, 0.0440),            # -0.4695 ± 0.0440    delta1
+            PD_Normal('gmr_qgamma0', -1.2595, 0.0026),            # -1.2595 ± 0.0026    beta0
+            PD_Normal('gmr_qgammaz', +0.0611, 0.0021),            # +0.0611 ± 0.0021    beta1
+            PD_Normal('gmr_qgammam', -0.0477, 0.0013),            # -0.0477 ± 0.0013    gamma
+
+            # MMBulge
+            # From [KH2013]_
+            PD_Normal('mmb_mamp_log10', 8.69, 0.05),              # 8.69 ± 0.05  [log10(M/Msol)]
+            PD_Normal('mmb_plaw', 1.17, 0.08),                    # 1.17 ± 0.08
+            # Extra
+            PD_Normal('mmb_scatter_dex', 0.28, 0.05),             # no uncertainties given
+            PD_Uniform('bf_frac_lo', 0.1, 0.4),
+            PD_Uniform('bf_frac_hi', 0.6, 1.0),
+            PD_Uniform('bf_width_dex', 0.5, 1.5),                 # [dex]
+        ]
+        _Param_Space.__init__(
+            self, parameters,
+            log=log, nsamples=nsamples, sam_shape=sam_shape, seed=seed,
+        )
+        return
 
 _param_spaces_dict = {
     'PS_Test': PS_Test,
@@ -914,4 +1084,5 @@ _param_spaces_dict = {
     'PS_Astro_Strong_ECG_13': PS_Astro_Strong_ECG_13,
     'PS_Astro_Strong_ECG_14': PS_Astro_Strong_ECG_14,
     'PS_Astro_Strong_ECG_15': PS_Astro_Strong_ECG_15,
+    'PS_Astro_Strong_ECG_17': PS_Astro_Strong_ECG_17,
 }
